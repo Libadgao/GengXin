0922b87decc8739c3932400b1eaeb3424cb1177e
Gao Yuan <1066347644@qq.com>
2018-09-03 15:30:15 +0800

    1.最新代码

--- app/src/main/java/bwei/com/myapplication/MainActivity.java
+++ app/src/main/java/bwei/com/myapplication/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("我做了一些改变");
         initView();
         initData();
     }
